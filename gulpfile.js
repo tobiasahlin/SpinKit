ba398fd0e303cff94a08b903de4f81f5aea47898
Jacob Magnusson <m@jacobian.se>
2014-09-14 09:07:51 +0200

    Inline all CSS into usage example HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-/*jshint multistr: true */
+'use strict';
 var _ = require('lodash');
 var fs = require('fs');
 var Q = require('q');
@@ -12,21 +12,26 @@ var htmlTmpl = '\n\
 <html>\n\
 <head>\n\
   <title><%= title %></title>\n\
-  <link rel="stylesheet" type="text/css" href="<%= cssFilename %>">\n\
+  <style type="text/css">\n\
+    <%= css %>\n\
+  </style>\n\
 </head>\n\
 <body>\n\
  <%= bodyContent %>\n\
 </body>\n\
 </html>';
 
+var getHtmlUsageExample = function(cssContent) {
+  return cssContent.match(/Usage:([\s\S]+\*.+\>)/)[1].replace(/ \*/g, '');
+};
+
 gulp.task('clean-styles', function() {
-  return gulp.src('./css', {read: false})
-             .pipe(rimraf());
+  return gulp.src('./css', {read: false}).pipe(rimraf());
 });
 
 
 gulp.task('styles', ['clean-styles'], function() {
-  return gulp.src('./scss/spinkit.scss')
+  return gulp.src('./scss/**/*.scss')
              .pipe(sass({errLogToConsole: true}))
              .pipe(autoprefixer('last 2 versions', {map: false}))
              .pipe(gulp.dest('./css'));
@@ -34,39 +39,40 @@ gulp.task('styles', ['clean-styles'], function() {
 
 
 gulp.task('clean-html', function() {
-  return gulp.src('./html', {read: false})
-             .pipe(rimraf());
+  return gulp.src('./html', {read: false}).pipe(rimraf());
 });
 
 
-// Generates HTML files from the usage examples found in the SCSS files
+// Generates HTML files from the usage examples found in the CSS files
 gulp.task('html', ['styles', 'clean-html'], function() {
-  var dirName = './scss/spinners/';
+  var cssDirName = './css/spinners/';
   var allDeferred = Q.defer();
 
   fs.mkdirSync('./html');
 
-  fs.readdir(dirName, function(err, filenames) {
+  fs.readdir(cssDirName, function(err, filenames) {
     var promises = filenames.map(function(filename) {
+      if (filename.indexOf('.css') === -1) { return; }
       var deferred = Q.defer();
-      var title = filename.substr(1).replace('.scss', '').replace(/-/g, ' ');
-      var htmlFilename = filename.substr(1).replace('.scss', '.html');
-      var readFile = function(err, content) {
-        if (err) { console.log(err); }
-        var bodyContent = content.match(/Usage:([\s\S]+\*.+\>)/)[1]
-                                 .replace(/ \*/g, '');
+      var title = filename.replace('.css', '').replace(/-/g, ' ');
+      var cssFilepath = cssDirName + filename;
+      var htmlFilename = filename.replace('.css', '.html');
+
+      var readCssFile = function(err, cssContent) {
+        if (err) { console.log(err); deferred.reject(err); }
+        var bodyContent = getHtmlUsageExample(cssContent);
         var html = _.template(htmlTmpl, {
-          cssFilename: '../css/spinkit.css',
+          css: cssContent,
           title: title,
           bodyContent: bodyContent
         });
         fs.writeFile('./html/' + htmlFilename, html, function(err, data) {
-          if (err) { console.log(err); }
+          if (err) { console.log(err); deferred.reject(err); }
           deferred.resolve();
         });
       };
 
-      fs.readFile(dirName + filename, {encoding: 'utf8'}, readFile);
+      fs.readFile(cssFilepath, {encoding: 'utf8'}, readCssFile);
       return deferred.promise;
     });
     Q.all(promises).then(allDeferred.resolve);
